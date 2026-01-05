298da05cde257f2e8abf3c5aae4ed0f04824061d
Amaara96 <amara.alex96@gmail.com>
2026-01-05 11:19:58 +0100

    vidtube

---
+++ src/models/user.models.js
@@ -0,0 +1,92 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary URL
+            required: true
+        },
+        coverImage: {
+            type: String, //cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    { timestamps: true }
+)
+
+// The prehooks(middleware)
+userSchema.pre("save", async function () {
+
+    if(!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10)
+
+
+})
+// to decrypt the password for comparison
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)  
+}
+
+// To generate access token while registering new user
+userSchema.methods.generateAccessToken = function (){
+    // short lived access token
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+);
+}
+
+// To generate refresh token while registering new user
+userSchema.methods.generateRefreshToken = function (){
+    // short lived access token
+    return jwt.sign({
+        _id: this._id,
+    }, 
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+);
+}
+
+
+export const User = mongoose.model("User", userSchema)
