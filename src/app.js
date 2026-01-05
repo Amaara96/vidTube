12172e8dfcd0f6a6f5f6cf32880714c71cc28e34
Amaara96 <amara.alex96@gmail.com>
2026-01-05 11:38:35 +0100

    Youtube commit

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express, { response } from "express";
+import logger from "../logger.js"
+import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
+
+const app = express();
+
+app.use(express.json())
+
+const morganFormat = 'method :url :status :response-time ms'
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject = {
+                method: message.split(' ')[0],
+                url: message.split(' ')[1],
+                status: message.split(' ')[2],
+                responseTime: message.split(' ')[3],
+            };
+            logger.info(JSON.stringify(logObject));
+        }
+    }
+}));
+
+
+// injecting common middlewares
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb"}))
+app.use(cookieParser()); 
+app.use(express.static("public"))
+
+
+
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import userRouter from "./routes/user.routes.js"
+
+// routes
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+
+
+export { app }
